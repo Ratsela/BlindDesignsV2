023ecb735a6c0caf07bfe128d69723dbfc1682e3
Ratsela <ratselamarch@gmail.com>
2014-09-04 12:50:31 +0200

    Blinds App

--- Tab1.js
+++ Tab1.js
@@ -190,3 +190,49 @@ $(document).off("pagecreate", "#Tab1").on("pagecreate", "#Tab1", function(event,
     Apperyio.processSelectMenu($(this));
     Tab1_js();
 });
+
+//Area Image JS
+
+
+	sliderInt=1;
+	roomId = 1;
+	count = $("#main_area >img").size();
+	$(document).ready(function(){
+		$("#main_area > img#1").fadeIn(600);
+	});
+	function next(){
+			count = $("#main_area >img").size();
+			//alert(''+count);
+			sliderInt += 1;
+			if(sliderInt <= count){
+				$("#main_area >img").fadeOut(600);
+				$("#main_area >img#" + sliderInt).fadeIn(600);
+				setRoomID(sliderInt);
+			}
+			else
+			{
+				alert("No more images");
+			}
+		}
+	function prev(){
+			sliderInt  = sliderInt - 1;
+			if(sliderInt > 0)
+			{
+				$("#main_area >img").fadeOut(600);
+				$("#main_area >img#" + sliderInt).fadeIn(600);
+				setRoomID(sliderInt);
+			}
+			else{
+				alert("No more images");
+			}
+		}
+	function setRoomID(id)
+	{
+		roomId = id;
+	}
+	function getRoomID()
+	{
+		return roomId;
+	}
+
+//Area Image JS
